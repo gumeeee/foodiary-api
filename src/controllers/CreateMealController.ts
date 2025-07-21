eb298e955db016af92222ac81af12af36d2ac057
gumeeee <gaxco123@gmail.com>
2025-07-20 22:45:02 -0300

    feat: add uploading to s3 in CreateMealController

--- src/controllers/CreateMealController.ts
+++ src/controllers/CreateMealController.ts
@@ -1,9 +1,12 @@
-import { eq } from "drizzle-orm";
-import { db } from "../db";
-import { HttpResponse, ProtectedHttpRequest } from "../types/Http";
-import { badRequest, created, ok } from "../utils/http";
-import { mealsTable, usersTable } from "../db/schema";
+import { PutObjectAclCommand } from "@aws-sdk/client-s3";
+import { randomUUID } from "node:crypto";
 import z from "zod";
+import { db } from "../db";
+import { mealsTable } from "../db/schema";
+import { HttpResponse, ProtectedHttpRequest } from "../types/Http";
+import { badRequest, created } from "../utils/http";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { s3Client } from "../clients/s3Client";
 
 const schema = z.object({
   fileType: z.enum(["audio/m4a", "image/jpeg"]),
@@ -20,6 +23,19 @@ export class CreateMealController {
       return badRequest({ errors: error.issues });
     }
 
+    const fileId = randomUUID();
+    const ext = data.fileType === "audio/m4a" ? ".m4a" : ".jpeg";
+    const fileKey = `${fileId}${ext}`;
+
+    const command = new PutObjectAclCommand({
+      Bucket: process.env.BUCKET_NAME,
+      Key: fileKey,
+    });
+
+    const presignedURL = await getSignedUrl(s3Client, command, {
+      expiresIn: 600,
+    });
+
     const [meal] = await db
       .insert(mealsTable)
       .values({
@@ -33,6 +49,6 @@ export class CreateMealController {
       })
       .returning({ id: mealsTable.id });
 
-    return created({ mealId: meal.id });
+    return created({ mealId: meal.id, uploadURL: presignedURL });
   }
 }
